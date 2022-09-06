e9ab3edf895fc0126fdd9497a0d1cac4e8a82759
Julien Esseiva <esseivaju@gmail.com>
2022-09-06 16:50:33 -0700

    double task duration

--- src/algorithms/AlgorithmA.cpp
+++ src/algorithms/AlgorithmA.cpp
@@ -9,5 +9,5 @@ namespace hpxdistributed::algorithms {
     Algorithm::StatusCode AlgorithmA::initialize() {
         return SUCCESS;
     }
-    AlgorithmA::AlgorithmA() : Algorithm(150, 1, "AlgorithmA") {}
+    AlgorithmA::AlgorithmA() : Algorithm(300, 1, "AlgorithmA") {}
 }// namespace hpxdistributed::algorithms
