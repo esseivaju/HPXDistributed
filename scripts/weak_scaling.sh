15b22281c9f5bf2e050d23efba64eac65cad7a70
Julien Esseiva <esseivaju@gmail.com>
2022-09-28 16:24:50 -0700

    update slurm scripts

--- scripts/weak_scaling.sh
+++ scripts/weak_scaling.sh
@@ -6,17 +6,15 @@
 #SBATCH -C cpu
 #SBATCH -N 40
 
-DBG=$1
-
-cd /global/homes/e/esseivaj/devel/HPXDistributed/build
-OUTPUT_DIR=/global/homes/e/esseivaj/devel/HPXDistributed/weak_scaling-$SLURM_JOBID
-
-mkdir $OUTPUT_DIR
-
 EVENTS_PER_CORE=10
 # 2 hyperthreads per core
 NCORES=$((${SLURM_CPUS_ON_NODE} / 2))
 NEVENTS_PER_NODE=$((${EVENTS_PER_CORE} * ${NCORES}))
+
+cd /global/homes/e/esseivaj/devel/HPXDistributed/build-5algs
+OUTPUT_DIR=/global/homes/e/esseivaj/devel/HPXDistributed/weak_scaling-numa_sensitive-${NCORES}cores-5algs-$SLURM_JOBID
+
+mkdir $OUTPUT_DIR
 
 # do cache warm-up
 for i in {2..6}; do
@@ -25,5 +23,5 @@ done
 
 for i in {40..2}; do
     NEVENTS=$(((${i} - 1) * ${NEVENTS_PER_NODE}))
-    srun -n${i} -N${i} HPXDistributed ${NEVENTS} --hpx:threads ${NCORES} -Ihpx.stacks.use_guard_pages=0 > $OUTPUT_DIR/${i}_nodes_${NEVENTS}_events
+    srun -n${i} -N${i} -c ${NCORES} --cpu-bind=sockets HPXDistributed ${NEVENTS} -Ihpx.stacks.use_guard_pages=0 --hpx:numa_sensitive=1 --hpx:print-counter-destination=$OUTPUT_DIR/perf_counters_$i --hpx:print-counter=/threads{locality#*/total}/idle-rate --hpx:print-counter=/threads{locality#*/total}/wait-time/pending > $OUTPUT_DIR/${i}_nodes_${NEVENTS}_events
 done
